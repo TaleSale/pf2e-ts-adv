74c4155aa543991eb20038fb4af1fce7f2e17b93
TaleSale <46991962+TaleSale@users.noreply.github.com>
2023-11-11 09:53:37 +0400

    5.3.9

--- priznak.js
+++ priznak.js
@@ -1,8 +1,11 @@
-
 Hooks.on("renderChatMessage", (message, html) => {
   const messageContent = html.find(".message-content");
   messageContent.html((index, oldHtml) => {
     // Используем регулярное выражение для поиска и замены текста внутри символов ``
-    return oldHtml.replace(/`([^`]+)`/g, '<span class="highlighted-text">`$1`</span>');
+    return oldHtml.replace(/`([^`"<>]+)`/g, '<u>`$1`</u>');    
   });
+
+  // Добавление стиля "ts-effect" только к <i> элементу внутри ссылки
+  messageContent.find("a i.fa-solid.fa-person-rays").addClass("ts-effect");
+  messageContent.find("a i.fas.fa-code").addClass("ts-macros");
 });
