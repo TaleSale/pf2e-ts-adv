be73bd70ffa5d6e7cc32f9d2cab36c92561c733f
TaleSale <46991962+TaleSale@users.noreply.github.com>
2023-11-18 20:23:19 +0400

    5.3.10

--- priznak.js
+++ priznak.js
@@ -2,7 +2,17 @@ Hooks.on("renderChatMessage", (message, html) => {
   const messageContent = html.find(".message-content");
   messageContent.html((index, oldHtml) => {
     // Используем регулярное выражение для поиска и замены текста внутри символов ``
-    return oldHtml.replace(/`([^`"<>]+)`/g, '<u>`$1`</u>');    
+    let newHtml = oldHtml.replace(/`([^`"<>]+)`/g, '<u>`$1`</u>');
+
+    // Замена шаблонов на обернутые в теги версии
+    newHtml = newHtml.replace(/(штраф[а-яё]*\s)(обстоятельств[а-яё]*)/g, '$1<tso>$2</tso>');
+    newHtml = newHtml.replace(/(бонус[а-яё]*\s)(обстоятельств[а-яё]*)/g, '$1<tso>$2</tso>');
+    newHtml = newHtml.replace(/(штраф[а-яё]*\s)(состоян[а-яё]*)/g, '$1<tss>$2</tss>');
+    newHtml = newHtml.replace(/(бонус[а-яё]*\s)(состоян[а-яё]*)/g, '$1<tss>$2</tss>');
+    newHtml = newHtml.replace(/(штраф[а-яё]*\s)(предмета)/g, '$1<tsp>$2</tsp>');
+    newHtml = newHtml.replace(/(бонус[а-яё]*\s)(предмета)/g, '$1<tsp>$2</tsp>');
+
+    return newHtml;
   });
 
   // Добавление стиля "ts-effect" только к <i> элементу внутри ссылки
