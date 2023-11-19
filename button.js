f2c8589ea37dcfcab053eeb915a14cec67c566e3
TaleSale <46991962+TaleSale@users.noreply.github.com>
2023-11-19 20:38:39 +0400

    5.3.11

--- button.js
+++
@@ -1,7 +0,0 @@
-Hooks.on('renderJournalSheet', (app, html, data) => {
-    const tsButton = $(`<button type="button" name="ts" title="TS"><i class="fas fa-music"></i>TS</button>`);
-    html.find('.editor-content').append(tsButton);
-    tsButton.click((ev) => {
-        // Вставьте здесь код, который должен выполняться при нажатии на кнопку
-    });
-});
