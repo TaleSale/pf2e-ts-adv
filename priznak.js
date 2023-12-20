3fa0aad1b691a64f519d82f06e47f8ddb5801c46
TaleSale <46991962+TaleSale@users.noreply.github.com>
2023-12-20 23:36:07 +0400

    5.4.4

--- priznak.js
+++ priznak.js
@@ -11,6 +11,18 @@ Hooks.on("renderChatMessage", (message, html) => {
     newHtml = newHtml.replace(/(бонус[а-яё]*\s)(состоян[а-яё]*)/g, '$1<tss>$2</tss>');
     newHtml = newHtml.replace(/(штраф[а-яё]*\s)(предмета)/g, '$1<tsp>$2</tsp>');
     newHtml = newHtml.replace(/(бонус[а-яё]*\s)(предмета)/g, '$1<tsp>$2</tsp>');
+    newHtml = newHtml.replace(/(Крит.Провал|Критический Провал|Крит.провал|Критический провал)(:)/g, '<kritproval>$1</kritproval>$2');
+    newHtml = newHtml.replace(/(Крит.Успех|Критический Успех|Крит.успех|Критический успех)(:)/g, '<krituspeh>$1</krituspeh>$2');
+    newHtml = newHtml.replace(/(Успех)(:)/g, '<uspeh>$1</uspeh>$2');
+    newHtml = newHtml.replace(/(Провал)(:)/g, '<proval>$1</proval>$2');
+    newHtml = newHtml.replace(/(Триггер)(:)/g, '<red>$1</red>$2');
+    newHtml = newHtml.replace(/(Требован[а-яё]*)(:)/g, '<darkblue>$1</darkblue>$2');
+    newHtml = newHtml.replace(/(Частота)(:)/g, '<green>$1</green>$2');
+    newHtml = newHtml.replace(/(Эффект)(:)/g, '<yellow>$1</yellow>$2');
+    newHtml = newHtml.replace(/(Усиление)(\s\([1-90+а-яё]*\))(:)/g, '<purple>$1$2</purple>$3');
+
+
+
 
     return newHtml;
   });
