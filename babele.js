d603fa4cb3dcb9aa50f787669cf10f7bd9f40925
TaleSale <46991962+TaleSale@users.noreply.github.com>
2024-06-22 13:11:54 +0400

    7.0.0

--- babele.js
+++ babele.js
@@ -1,88 +1,37 @@
-// Функция для проверки наличия перевода
-function checkTranslation(_this, data) {
-    if (!!_this.translations[data._id]) return true;
-
-    if (data.name?.length > 1 && data.name.indexOf('(') > -1 && data.name.indexOf(')') > -1) {
-        const match = /(.+)\s\((At Will|Constant)\)$/.exec(data.name);
-        if (match) {
-            const name = match[1].trim();
-            if (!!_this.translations[name]) return true;
-        }
+// Слушаем клики на весь документ
+$(document).on('click', '.read', function(event) {
+    // Получаем размеры элемента
+    const elementWidth = this.clientWidth;
+    const elementHeight = this.clientHeight;
+    
+    // Определяем координаты клика
+    const clickX = event.offsetX;
+    const clickY = event.offsetY;
+    
+    // Zадаем границы для активной области клика
+    const activeClickWidth = elementWidth - 10; // клик в последние 100px ширины
+    const activeClickHeight = 10;               // клик в первые 100px высоты
+    
+    // Проверяем, что клик был в установленной области
+    if (clickX > activeClickWidth && clickY < activeClickHeight) {
+      let sectionContent = this.innerHTML;
+      
+      // Создание объекта данных для сообщения в чат
+      let chatData = {
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker(),
+        content: sectionContent
+      };
+      
+      // Отправка сообщения в чат
+      ChatMessage.create(chatData);
     }
-
-    return false;
-}
-
-async function __PF2E_TS_init(_this, wrapped, ...args) {
-    await wrapped();
-
-    return new Promise((resolve, reject) => {
-        for (let metadata of game.data.packs) {
-            const collection = `${metadata.packageName}.${metadata.name}`;
-            if(_this.supported(metadata)) {
-                const translatedCompendium = _this.packs.get(collection);
-                const variants = _this.translations.find(t => t.collection === collection)?.variants || [];
-                if (translatedCompendium && variants?.length > 0) {
-                    translatedCompendium.variants = variants;
-                    _this.packs.set(collection, translatedCompendium);
-                }
-            }
-        }
-        resolve();
-    });
-};
-
-function __PF2E_TS_hasTranslation(_this, wrapped, data) {
-    return checkTranslation(_this, data) ? true : wrapped(data);
-};
-
-function __PF2E_TS_translationsFor(_this, wrapped, data) {
-    if (checkTranslation(_this, data)) {
-        const match = /(.+)\s\((At Will|Constant)\)$/.exec(data.name);
-        if (match) {
-            const name = match[1].trim();
-            const variant = match[2].trim();
-            let data = Object.assign({}, _this.translations[name]);
-            const translatedName = data.name;
-            const translatedVariant = `(${variant})`;
-            
-            if (data['variants'] || _this.variants) {
-                for (let v of data['variants'] || _this.variants || []) {
-                    if (v.id === variant) {
-                        data = Object.assign(data, v);
-                        break;
-                    }
-                }
-            }
-
-            data.name = `${translatedName} ${translatedVariant}`;
-            return data;
-        }
-    }
-
-    return wrapped(data);
-}
+  });
 
 Hooks.on('init', () => {
-    if (typeof libWrapper === 'function') {
-        libWrapper.register('pf2e-ts-adv', 'Babele.prototype.init', async function (wrapped, ...args) {
-            return __PF2E_TS_init(this, wrapped, ...args);
-        }, 'WRAPPER');
-        libWrapper.register('pf2e-ts-adv', 'TranslatedCompendium.prototype.hasTranslation', function (wrapped, ...args) {
-            return __PF2E_TS_hasTranslation(this, wrapped, ...args);
-        }, 'MIXED');
-        libWrapper.register('pf2e-ts-adv', 'TranslatedCompendium.prototype.translationsFor', function (wrapped, ...args) {
-            return __PF2E_TS_translationsFor(this, wrapped, ...args);
-        }, 'MIXED');
-    }
-
-    if(typeof Babele !== 'undefined') {
-        Babele.get().register({
-            module: 'pf2e-ts-adv',
-            lang: 'ru',
-            dir: 'compendium'
-        });
-    }
+    Babele.get().register({
+        module: 'pf2e-ts-adv',
+        lang: 'ru',
+        dir: 'compendium'
+    });
 });
-
- 
