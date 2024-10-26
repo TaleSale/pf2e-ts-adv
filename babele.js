0927cbe33a07d2a41e230857f4a7effd35894959
TaleSale <46991962+TaleSale@users.noreply.github.com>
2024-10-26 10:25:35 +0400

    7.2.0

--- babele.js
+++ babele.js
@@ -1,37 +1,37 @@
 // Слушаем клики на весь документ
 $(document).on('click', '.read', function(event) {
-    // Получаем размеры элемента
-    const elementWidth = this.clientWidth;
-    const elementHeight = this.clientHeight;
+  // Получаем размеры элемента
+  const elementWidth = this.clientWidth;
+  const elementHeight = this.clientHeight;
+  
+  // Определяем координаты клика
+  const clickX = event.offsetX;
+  const clickY = event.offsetY;
+  
+  // Zадаем границы для активной области клика
+  const activeClickWidth = elementWidth - 10; // клик в последние 100px ширины
+  const activeClickHeight = 10;               // клик в первые 100px высоты
+  
+  // Проверяем, что клик был в установленной области
+  if (clickX > activeClickWidth && clickY < activeClickHeight) {
+    let sectionContent = this.innerHTML;
     
-    // Определяем координаты клика
-    const clickX = event.offsetX;
-    const clickY = event.offsetY;
+    // Создание объекта данных для сообщения в чат
+    let chatData = {
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker(),
+      content: sectionContent
+    };
     
-    // Zадаем границы для активной области клика
-    const activeClickWidth = elementWidth - 10; // клик в последние 100px ширины
-    const activeClickHeight = 10;               // клик в первые 100px высоты
-    
-    // Проверяем, что клик был в установленной области
-    if (clickX > activeClickWidth && clickY < activeClickHeight) {
-      let sectionContent = this.innerHTML;
-      
-      // Создание объекта данных для сообщения в чат
-      let chatData = {
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker(),
-        content: sectionContent
-      };
-      
-      // Отправка сообщения в чат
-      ChatMessage.create(chatData);
-    }
-  });
+    // Отправка сообщения в чат
+    ChatMessage.create(chatData);
+  }
+});
 
 Hooks.on('init', () => {
-    Babele.get().register({
-        module: 'pf2e-ts-adv',
-        lang: 'ru',
-        dir: 'compendium'
-    });
+  Babele.get().register({
+      module: 'pf2e-ts-adv',
+      lang: 'ru',
+      dir: 'compendium'
+  });
 });
